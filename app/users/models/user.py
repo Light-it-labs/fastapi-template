781455fef68d58a19840e745b268a0de692078ca
Stéfano Troncaro <stefano.troncaro@lightit.io>
2025-04-04 15:20:00 -0300

    fix: long emails breaking user create endpoint

--- app/users/models/user.py
+++ app/users/models/user.py
@@ -2,8 +2,10 @@ from sqlalchemy import Column, String
 
 from app.common.models.base_class import Base
 
+from app.users.constants.user_constants import USER_EMAIL_MAX_LENGTH
+
 
 class User(Base):
     __tablename__ = "users"
-    email = Column(String(100), unique=True, nullable=False)
+    email = Column(String(USER_EMAIL_MAX_LENGTH), unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
