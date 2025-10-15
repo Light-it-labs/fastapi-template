6ce1fa4ab57a7eccc271fb8ab1bfff9b79fcd6d1
Stéfano Troncaro <stefano.troncaro@lightit.io>
2025-10-15 11:09:20 -0300

    Base model and Alembic upgrades (#27)
    * feat: update base class with type annotation map and naming convention
    
    * fix: missing timezone=True from 2fa migration
    
    * fix: old migrations to follow constraint names
    
    * fix: alembic deleting celery tables on revision autogeneration
    
    * fix: alembic env type annotations
    
    * fix: redo migrations
    
    * refactor: simplify boolean expression

--- app/common/models/base_class.py
+++ app/common/models/base_class.py
@@ -1,17 +1,31 @@
 from uuid import UUID, uuid4
 from datetime import datetime
 
-from sqlalchemy import func
+from sqlalchemy import MetaData, func, UUID as SQLA_UUID, DateTime
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
     mapped_column,
-    declared_attr,
 )
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+
+    type_annotation_map = {
+        UUID: SQLA_UUID,
+        datetime: DateTime(timezone=True),
+    }
 
     id: Mapped[UUID] = mapped_column(
         default=uuid4,
@@ -22,9 +36,5 @@ class Base(DeclarativeBase):
     )
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(),
-        onupdate=func.now(),
+        server_onupdate=func.now(),
     )
-
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
