49919f3409f3c2a3057d8993af6464fe382cdfc4
Stéfano Troncaro <stefano.troncaro@lightit.io>
2025-11-28 12:11:02 -0300

    feat: extract template layout

--- app/users/schemas/templates.py
+++ app/users/schemas/templates.py
@@ -1,5 +1,5 @@
 from app import templates
 
 
-class NewUserTemplate(templates.BaseEmailTemplate, path="base.mjml"):
+class NewUserTemplate(templates.BaseEmailTemplate, path="new_user_email.mjml"):
     name: str
