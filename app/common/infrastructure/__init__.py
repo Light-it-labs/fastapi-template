1f823a945973e5bb4ba1ab2f469a76d89e10eb23
Stéfano Troncaro <stefano.troncaro@lightit.io>
2026-01-29 19:07:41 -0300

    feat: common infrastructure facade

---
+++ app/common/infrastructure/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "BaseSQLAlchemyModel",
+    "BaseSQLAlchemyRepository",
+    "SqlalchemyCriteriaTranslator",
+)
+
+from .base_sqlalchemy_model import BaseSQLAlchemyModel
+from .base_sqlalchemy_repository import BaseSQLAlchemyRepository
+from .sqlalchemy_criteria_translator import SqlalchemyCriteriaTranslator
